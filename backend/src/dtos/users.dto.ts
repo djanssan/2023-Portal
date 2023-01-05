5ac3a86bd857212effbd509a3921d82626cd4f44
djanssan <111241799+djanssan@users.noreply.github.com>
2023-01-05 12:58:47 +0000

    Add files via upload

---
+++ backend/src/dtos/users.dto.ts
@@ -0,0 +1,23 @@
+import { IsEmail, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  public email: string;
+
+  @IsString()
+  public password: string;
+
+	@IsString()
+  public firstName: string;
+
+	@IsString()
+  public lastName: string;
+}
+
+export class LoginUserDto {
+  @IsEmail()
+  public email: string;
+
+  @IsString()
+  public password: string;
+}
